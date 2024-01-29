91cd58a742443927e0989f2e5697a405f989540c
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-01-29 11:24:29 +0200

    add

--- components/components-demo/src/app/app.component.ts
+++ components/components-demo/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './about/about.component'
 import {NavigationComponent} from './navigation/navigation.component'
+import { CommonModule } from '@angular/common';  
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent,AboutComponent],
+  imports: [RouterOutlet,CommonModule, NavigationComponent,AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- components/components-demo/src/app/navigation/navigation.component.ts
+++ components/components-demo/src/app/navigation/navigation.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';  
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
+  title ='hide'
+  toggle =false;
+  activeUsers = [{name:"martin", age:16},{name:"bobi", age:41},{name:"Kalin", age:15}]
   navigationClickMe (){
-    console.log('navigationClickMe')
-  }
+this.toggle = !this.toggle  
+}
 }
