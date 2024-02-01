34cd536e77b4ef7c4e040b9b9fc2c805920698b4
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-01 16:31:20 +0200

    Update user.service.ts

--- DI, Intro to RxJS, Services/angular-app/src/app/user.service.ts
+++ DI, Intro to RxJS, Services/angular-app/src/app/user.service.ts
@@ -7,24 +7,24 @@ import { User } from './Type/User';
 export class UserService {
 
   constructor() {
-    setInterval(() =>{
+    setInterval(() => {
 
       this.users.push({
         name: 'Demo',
         age: 21
       })
       console.log('Demo is added')
-    },5000)
+    }, 5000)
   }
-  users: User [] = [{ name: 'John', age: 21 }, { name: 'Huicho', age: 61 }, { name: 'Atanas', age: 77 }, { name: 'Peter', age: 32 }]
+  users: User[] = [{ name: 'John', age: 21 }, { name: 'Huicho', age: 61 }, { name: 'Atanas', age: 77 }, { name: 'Peter', age: 32 }]
 
-  addUser (inputName:HTMLInputElement, inputAge:HTMLInputElement){
+  addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
     const user = {
       name: inputName.value,
       age: +inputAge.value
     }
-    this.users= [...this.users, user];
-    inputName.value="";
-    inputAge.value="";
+    this.users = [...this.users, user];
+    inputName.value = "";
+    inputAge.value = "";
   }
 }
