276315e05e078193aebe141246d3fca0152c8894
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-02 10:27:29 +0200

    add

--- workshop-components/components-workshop/src/app/api.service.ts
+++ workshop-components/components-workshop/src/app/api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,14 @@ import { Injectable } from '@angular/core';
 export class ApiService {
 
   constructor(private http: HttpClient) { }
-  getThemes() {}
-  getPosts() {}
+  getThemes() {
+    const {appUrl} = environment
+    return this.http.get(`${appUrl}/themes`)
+  }
+  getPosts(limit?: number) {
+    const {appUrl} = environment
+    const limitFilter = limit ? `?limit=${limit}` :'';
+    return this.http.get(`${appUrl}/posts`)
+
+  }
 }
