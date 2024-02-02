d315eeb053b79900c689d0776afaec1f71d8e8a7
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-02 11:29:45 +0200

    add

--- workshop-components/components-workshop/src/app/types/theme.ts
+++ workshop-components/components-workshop/src/app/types/theme.ts
@@ -1,2 +1,13 @@
-export interface Theme{}
+import { UserId } from "./userID";
+
+export interface Theme{
+    "subscribers": string[];
+    "posts": string[];
+    "_id": string
+    "themeName": string
+    "userId": UserId;
+    "created_at": string;
+    "updatedAt": string
+    "__v": number
+}
 
