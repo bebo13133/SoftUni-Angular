6e6ac9c9032b51b22d2ae7a098cca6184109fcde
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-07 21:38:43 +0200

    create home login register

--- forms/directives-forms/src/app/hightlight-on-move.directive.ts
+++ forms/directives-forms/src/app/hightlight-on-move.directive.ts
@@ -1,35 +1,43 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHightlightOnMove]',
   standalone: true
 })
-export class HightlightOnMoveDirective implements OnInit {
+export class HightlightOnMoveDirective implements OnInit, OnDestroy {
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
-
+  unsubScribe: (() => void)[] = []
   ngOnInit(): void {
     //   this.elRef.nativeElement.style.backgroundColor= "red"
     // this.renderer.setStyle(this.elRef.nativeElement, "backgroundColor","blue")
-    this.renderer.listen(
+    this.unsubScribe.push(this.renderer.listen(
       this.elRef.nativeElement,
       "mouseenter",
       this.mouseEnterEven.bind(this)
-    )
-    this.renderer.listen(
-      this.elRef.nativeElement,
-      "mouseleave",
-      this.mouseLeaveEven.bind(this)
-    )
+    ))
+
+    this.unsubScribe.push(
+      this.renderer.listen(
+        this.elRef.nativeElement,
+        "mouseleave",
+        this.mouseLeaveEven.bind(this)
+      ))
+
   }
+
   mouseEnterEven(event: MouseEvent): void {
     console.log("enter", event)
-    this.renderer.setStyle(this.elRef.nativeElement, "backgroundColor","red")
+    this.renderer.setStyle(this.elRef.nativeElement, "backgroundColor", "red")
 
   }
   mouseLeaveEven(event: MouseEvent): void {
-    this.renderer.setStyle(this.elRef.nativeElement, "backgroundColor","blue")
+    this.renderer.setStyle(this.elRef.nativeElement, "backgroundColor", "blue")
 
     console.log("enter", event)
+  }
+
+  ngOnDestroy(): void {
+    this.unsubScribe.forEach((fn)=>fn())
   }
 }
