00bfb51155887cafde0550a80f2e5894ea337924
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-08 09:40:47 +0200

    add

--- forms/directives-forms/src/app/app.component.ts
+++ forms/directives-forms/src/app/app.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HightlightOnMoveDirective } from './hightlight-on-move.directive';
 import { MyRouterLinkDirective } from './my-router-link.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HightlightOnMoveDirective, MyRouterLinkDirective],
+  imports: [RouterOutlet, HightlightOnMoveDirective, MyRouterLinkDirective, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  isShown = false;
   title = 'directives-forms';
+  toggle(): void {
+    this.isShown = !this.isShown;
+  }
 }
