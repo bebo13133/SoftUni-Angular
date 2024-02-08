0cef73613b453a4464cf4ed0064259a1718edc1b
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-08 07:40:52 +0200

    push directives

---
+++ forms/directives-forms/src/app/my-router-link.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appMyRouterLink]',
+  standalone: true
+})
+export class MyRouterLinkDirective {
+
+  constructor() { }
+
+}
