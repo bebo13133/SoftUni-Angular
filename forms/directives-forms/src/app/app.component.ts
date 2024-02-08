63b1592716d8a169f49fe72d2bfbb6751afd1d7c
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-08 09:26:24 +0200

    add

--- forms/directives-forms/src/app/app.component.ts
+++ forms/directives-forms/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HightlightOnMoveDirective } from './hightlight-on-move.directive';
+import { MyRouterLinkDirective } from './my-router-link.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HightlightOnMoveDirective],
+  imports: [RouterOutlet,HightlightOnMoveDirective,MyRouterLinkDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- forms/directives-forms/src/app/my-router-link.directive.ts
+++ forms/directives-forms/src/app/my-router-link.directive.ts
@@ -1,11 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Directive({
   selector: '[appMyRouterLink]',
   standalone: true
 })
-export class MyRouterLinkDirective {
+export class MyRouterLinkDirective implements OnInit {
+@Input() appMyRouterLink: string = ''
+  constructor(private el: ElementRef, private render: Renderer2, private router: Router) { }
 
-  constructor() { }
-
+  ngOnInit(): void {
+    this.render.listen(
+      this.el.nativeElement,
+      'click',
+      this.clickHandler.bind(this)
+    )
+  }
+  clickHandler(event: MouseEvent): void {
+    this.router.navigate([this.appMyRouterLink])
+  }
 }
