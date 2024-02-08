4c3f680ed29f3c20bec5daa9961b5668bd1e0f36
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-08 18:52:12 +0200

    адд

--- forms/directives-forms/src/app/login/login.component.ts
+++ forms/directives-forms/src/app/login/login.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { MaxCountDirective } from '../max-count.directive';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,MaxCountDirective],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
