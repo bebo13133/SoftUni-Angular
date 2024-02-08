87a600c42e1c902633dfb86f88deae0f02eacfa1
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-08 08:12:51 +0200

    add

--- forms/directives-forms/src/app/app.routes.ts
+++ forms/directives-forms/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        pathMatch: 'full',
+        redirectTo: '/home'
+    },
+    {
+        path:'home',
+        component: HomeComponent
+    },
+    {
+        path:'login',
+        component: LoginComponent,
+
+    },
+    {
+        path:'register',
+        component: RegisterComponent
+    }
+];
