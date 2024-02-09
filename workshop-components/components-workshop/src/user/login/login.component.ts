7bf4a02acafd0ca628c1ace8a5c06ea5db272205
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-09 09:20:56 +0200

    add

--- workshop-components/components-workshop/src/user/login/login.component.ts
+++ workshop-components/components-workshop/src/user/login/login.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
 constructor(private userService: UserService, private router:Router) {}
-  login(email:string, password:string):void {
+  login(form:NgForm):void {
+    console.log('form', form.value);
 
 
     this.userService.login()
