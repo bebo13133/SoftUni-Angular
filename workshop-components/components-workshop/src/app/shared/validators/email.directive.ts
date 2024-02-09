2ba2e17b75591bf536ecb384053e8b6f68e7384f
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-09 10:27:57 +0200

    add

--- workshop-components/components-workshop/src/app/shared/validators/email.directive.ts
+++ workshop-components/components-workshop/src/app/shared/validators/email.directive.ts
@@ -1,17 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { AbstractControl, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
   selector: '[appEmail]',
   standalone: true
 })
-export class EmailDirective implements Validator {
-
+export class EmailDirective implements Validator,OnChanges {
+@Input() appEmail: string[] =[]
   constructor() { }
 validate(control: AbstractControl<any, any>): ValidationErrors | null {
 
 
-  
+
   return null
+}
+ngOnChanges(changes: SimpleChanges): void {
+  
 }
 }
