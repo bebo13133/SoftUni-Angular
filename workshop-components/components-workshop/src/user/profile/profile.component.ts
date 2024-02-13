0465608327019745f3db8a15939936401ec49554
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-13 10:49:31 +0200

    add

--- workshop-components/components-workshop/src/user/profile/profile.component.ts
+++ workshop-components/components-workshop/src/user/profile/profile.component.ts
@@ -1,18 +1,27 @@
 import { isNgTemplate } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DEFAULT_EMAIL_DOMAINS } from '../../app/shared/constants';
 import { appEmailValidator } from '../../app/shared/validators';
 import { UserService } from '../user.service';
+import { CommonModule } from '@angular/common';
 
+
+interface Profile {
+  username: string;
+  email: string;
+  tel: string;
+  ext: string;
+}
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit {
+  isEditMode:boolean = false;
 
   form = this.fb.group({
     username: ['', [Validators.required, Validators.minLength(5)]],
@@ -22,21 +31,36 @@ export class ProfileComponent implements OnInit {
 
   })
 
-  get user() {
-    const { username, email, tel: telephone } = this.userService.user!;
-    // const [ext, ...tel] = telephone.split(' ');
-    return {
-      username,
-      email,
-      // tel: tel.join(' '),
-      // ext,
-    }
-  }
+  profileDetails : Profile |undefined
+
+
+  // get user() {
+  //   const { username, email, tel: telephone } = this.userService.user!;
+  //   // const [ext, ...tel] = telephone.split(' ');
+  //   return {
+  //     username,
+  //     email,
+  //     // tel: tel.join(' '),
+  //     // ext,
+  //   }
+  // }
+
+
   constructor(private fb: FormBuilder, private userService: UserService) {
 
   }
+  toggleEditMode():void {
+    this.isEditMode = !this.isEditMode;
+  }
+  onEditHandler():void {
+    if(this.form.invalid) {return;}
+    this.profileDetails = {...this.form.value} as Profile
 
+    console.log(this.form.value)
+    this.toggleEditMode()
+  };
   ngOnInit(): void {
 
   }
+  
 }
