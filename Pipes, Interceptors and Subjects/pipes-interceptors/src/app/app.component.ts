808114faa730fa0f8fd5a7d062624989c505c7f5
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-27 19:45:09 +0200

    адд

--- Pipes, Interceptors and Subjects/pipes-interceptors/src/app/app.component.ts
+++ Pipes, Interceptors and Subjects/pipes-interceptors/src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReducePipe } from './reduce.pipe';
 import { interval, map } from 'rxjs';
+import { UserService } from './user.service';
+
+import { AppInterceptor, apiInterceptorProvider } from './app.interceptor';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,CommonModule, ReducePipe],
+  providers:[AppInterceptor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // title = 'pipes-interceptors';
   user = {name: "bobi" , age:41, list:[1,2,3,4,5]}
   sum(a:number,b:number):number{
@@ -23,4 +27,10 @@ export class AppComponent {
     console.log(this.user)
   }
   date$ = interval (1000).pipe(map(()=> new Date()))
+constructor (private userService: UserService){}
+  ngOnInit(): void {
+    this.userService.loadUsers().subscribe({
+      next: console.log,
+    })
+  }
 }
---
+++ Pipes, Interceptors and Subjects/pipes-interceptors/src/app/app.interceptor.ts
@@ -0,0 +1,25 @@
+import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable, tap } from "rxjs";
+import { API_URL } from "./constants";
+import { Provider } from "@angular/core";
+
+export class AppInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        
+        let request = req
+        if(req.url.startsWith('/api')){
+            request = req.clone({
+                url: req.url.replace('/api',API_URL)
+            })
+        }
+        return next.handle(request).pipe(tap((req)=>{
+            console.log(req)
+        }))
+    }
+
+}
+export const apiInterceptorProvider: Provider ={
+    provide: HTTP_INTERCEPTORS,
+    multi:true,
+    useClass: AppInterceptor
+}
