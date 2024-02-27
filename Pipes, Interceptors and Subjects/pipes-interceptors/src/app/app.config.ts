fa502758366e45b44f145fa52f3a884ec8a10187
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-27 19:58:14 +0200

    Update app.config.ts

--- Pipes, Interceptors and Subjects/pipes-interceptors/src/app/app.config.ts
+++ Pipes, Interceptors and Subjects/pipes-interceptors/src/app/app.config.ts
@@ -2,9 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { apiInterceptorProvider } from './app.interceptor';
-import { provideHttpClient } from '@angular/common/http';
+import { AppInterceptor, apiInterceptorProvider } from './app.interceptor';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(),]
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([AppInterceptor]))]
 };
