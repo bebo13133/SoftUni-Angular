d65dc049f68159491eb40e1e39f911e7f844266e
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-02-27 11:28:04 +0200

    add

--- Pipes, Interceptors and Subjects/pipes-interceptors/src/app/app.component.ts
+++ Pipes, Interceptors and Subjects/pipes-interceptors/src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReducePipe } from './reduce.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule, ReducePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipes-interceptors';
+  // title = 'pipes-interceptors';
+  user = {name: "bobi" , age:41, list:[1,2,3,4,5]}
+  sum(a:number,b:number):number{
+    return a+b
+  }
+
 }
---
+++ Pipes, Interceptors and Subjects/pipes-interceptors/src/app/reduce.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'reduce',
+  standalone: true
+})
+export class ReducePipe <T>implements PipeTransform {
+
+  transform(array: T[],reduceFn:(acc:any, curr:T)=> any, initialValue:T): unknown {
+    return array.reduce(reduceFn,initialValue);
+  }
+
+}
